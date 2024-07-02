43c27eb4ace31753c560262528142628917d0d6f
Felix <felixsavero78@gmail.com>
2024-07-02 15:34:09 +0700

    feat: validator message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,12 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    public function testValidator()
+    public function testValidatorValid()
     {
         $data = [
             'username' => 'admin',
@@ -22,6 +23,33 @@ class ValidatorTest extends TestCase
         ];
 
         $validator = Validator::make($data, $rules);
+
         $this->assertNotNull($validator);
+        $this->assertTrue($validator->passes());
+        $this->assertFalse($validator->fails());
+    }
+
+    public function testValidatorInvalid()
+    {
+        $data = [
+            'username' => '',
+            'password' => ''
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        $this->assertNotNull($validator);
+        $this->assertFalse($validator->passes());
+        $this->assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+        $message->get('username');
+        $message->get('password');
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
