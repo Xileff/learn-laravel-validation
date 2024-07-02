1fb42deaff9f5fb1912a11d55dec1809875756c3
Felix <felixsavero78@gmail.com>
2024-07-02 17:42:02 +0700

    feat: HTTP request validation

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+
+class FormController extends Controller
+{
+    public function login(Request $request): Response
+    {
+        try {
+            $rules = [
+                'username' => 'required',
+                'password' => 'required'
+            ];
+
+            $data = $request->validate($rules);
+
+            return response("OK", RESPONSE::HTTP_OK);
+        } catch (ValidationException $e) {
+            return response($e->errors(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
